5b17d74b7f27a86bad8450c56538f399c12382bd
javjuarez <sonico@ciencias.unam.mx>
2022-04-03 21:04:43 -0500

    - Íconos agregados y usados - Barra de búsqueda agregada - Pantalla de ingreso de datos habilitada - Pantalla inicial mostrada - Colores personalizados conforme a los mockups TODO - Paso de parámetros de un productor a una pantalla con sus datos registrados - Definir modelo para los usuarios

---
+++ app/src/main/java/com/example/yemaapp/DatosUsuario.kt
@@ -0,0 +1,51 @@
+package com.example.yemaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.example.yemaapp.databinding.ActivityDatosUsuarioBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+
+class DatosUsuario : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDatosUsuarioBinding
+    private lateinit var datePicker: MaterialDatePicker<Long>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDatosUsuarioBinding.inflate(layoutInflater)
+
+        with(binding) {
+            setContentView(root)
+            setSupportActionBar(appToolbar.appToolbar)
+            appToolbar.appToolbar.navigationIcon =
+                ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_arrow_back_24, null)
+
+            appToolbar.appToolbar.setNavigationOnClickListener {
+                startActivity(Intent(this@DatosUsuario, MainActivity::class.java))
+                finish()
+            }
+
+            scrollDatosRegistro.textInputEditTextFechaNac.setOnClickListener {
+                datePicker = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Fecha de nacimiento")
+                    .build()
+                datePicker.show(supportFragmentManager, datePicker.toString())
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
--- app/src/main/java/com/example/yemaapp/MainActivity.kt
+++ app/src/main/java/com/example/yemaapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.yemaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.yemaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        with(binding) {
+            setContentView(root)
+            setSupportActionBar(appToolbar.appToolbar)
+
+            appToolbar.appToolbar.setNavigationOnClickListener {
+                startActivity(Intent(this@MainActivity, DatosUsuario::class.java))
+                finish()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
